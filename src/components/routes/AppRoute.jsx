dcba607004ff6ff2b2ed8ba32221e2dd989157a2
alxcyha <aliciamargaret2192@gmail.com>
2023-06-13 13:52:55 +0800

    React.lazy

--- src/components/routes/AppRoute.jsx
+++ src/components/routes/AppRoute.jsx
@@ -1,14 +1,16 @@
+import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import HomeAdmin from "../pages/Home";
-import Login from "../pages/Login";
+
+const Login = React.lazy(()=> import("../pages/Login"));
+const HomeAdmin = React.lazy(()=> import("../pages/Home"));
 
 export default function AppRoute(){
     return(
         <>
             <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />}> </Route>
-                <Route path="/home" element={<HomeAdmin />}> </Route>
+                <Route path="/" element={<Login />}/> 
+                <Route path="/home" element={<HomeAdmin />}/>
             </Routes>
             </BrowserRouter>
         </>
