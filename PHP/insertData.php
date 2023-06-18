37d974a0b98250989b187614cea1280f8ad40fed
Kent <yagongkent@gmail.com>
2023-06-18 09:58:36 +0800

    AUDIT FRONT END AND BACKEND COMPLETE!

---
+++ PHP/insertData.php
@@ -0,0 +1,57 @@
+<?php
+// Allow requests from any origin
+header('Access-Control-Allow-Origin: *');
+
+// Allow specific HTTP methods (GET in this case)
+header('Access-Control-Allow-Methods: GET');
+
+// Allow specific headers
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Check if the request method is OPTIONS (preflight request)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  // Return early for preflight requests
+  exit;
+}
+
+// Check if the request method is GET
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  // Perform any necessary validation or processing
+
+  // Insert the data into the database or perform other actions as needed
+  // Example: Fetching data from a MySQL database using PDO
+  $dsn = 'mysql:host=localhost;dbname=hmstry';
+  $username = 'root';
+  $dbPassword = '';
+
+  try {
+    $pdo = new PDO($dsn, $username, $dbPassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $pdo->prepare('SELECT * FROM all_doctors');
+
+    // Execute the prepared statement
+    $stmt->execute();
+
+    // Fetch all rows from the result set as an associative array
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Return the fetched data as JSON response
+    header('Content-Type: application/json');
+    echo json_encode($result);
+  } catch (PDOException $e) {
+    // Return an error response
+    $response = [
+      'success' => false,
+      'message' => 'Error: ' . $e->getMessage()
+    ];
+    echo json_encode($response);
+  }
+} else {
+  // Return an error response if the request method is not GET
+  $response = [
+    'success' => false,
+    'message' => 'Invalid request method'
+  ];
+  echo json_encode($response);
+}
---
+++ TEST/audit.jsx
@@ -0,0 +1,65 @@
+import './App.css';
+import { Table } from 'reactstrap';
+import React, { useState, useEffect } from 'react';
+
+function InsertTable() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = () => {
+    fetch('http://localhost/HMS/PHP/insertData.php')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Invalid request.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setUsers(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setError('Error fetching data: ' + error.message);
+        setLoading(false);
+      });
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <>
+      <Table>
+        <thead>
+          <tr>
+            <th>Doctor ID</th>
+            <th>Doctor Name</th>
+            <th>Department</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, key) => (
+            <tr key={key}>
+              <td>{user.doctor_id}</td>
+              <td>{user.doctor_name}</td>
+              <td>{user.department_name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+
+export default InsertTable;
