441eca8437aeac6a237623a42aefce1c88d096a1
alxcyha <aliciamargaret2192@gmail.com>
2023-06-23 18:32:14 +0800

    update label sign in

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -83,7 +83,7 @@ function LoginDoctor() {
               <div className="row d-flex justify-content-center">
                 <div className="col-lg-10">
                   <h2 className="fw-bold mt-3"></h2>
-                  <h2 className="mb-4">Sign In</h2>
+                  <h2 className="mb-4"> Admin Sign In</h2>
                   <Form
                     action="http://localhost/HMS/PHP/server.php"
                     method="post"
