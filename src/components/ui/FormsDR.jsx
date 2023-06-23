e1f069739f2228ec82a6f87c610b907cdee3aff4
Kent <yagongkent@gmail.com>
2023-06-23 16:23:07 +0800

    change

--- src/components/ui/FormsDR.jsx
+++ src/components/ui/FormsDR.jsx
@@ -1,69 +1,73 @@
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/RegForm.css";
-import { 
-    Button, 
-    Form, 
-    FormGroup, 
-    Label, 
-    Input, 
-    Row, 
-    Col, 
-    Nav,
-    NavItem,
-    NavLink,
-    TabContent,
-    TabPane} from 'reactstrap';
-import { Box } from '@mui/material';
-import classnames from 'classnames';
-import RegFormDR from './RegForms/RegFormDR';
-import Doc_On_Call from './RegForms/Doc_On_Call';
-
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Row,
+  Col,
+  Nav,
+  NavItem,
+  NavLink,
+  TabContent,
+  TabPane,
+} from "reactstrap";
+import { Box } from "@mui/material";
+import classnames from "classnames";
+import RegFormDR from "./RegForms/RegFormDR";
+import Doc_On_Call from "./RegForms/Doc_On_Call";
 
 export function FormTabs(props) {
-    // State for current active Tab
-    const [currentActiveTab, setCurrentActiveTab] = useState('1');
-    // Toggle active state for Tab
-    const toggle = tab => {
-        if (currentActiveTab !== tab) setCurrentActiveTab(tab);
-    }
+  // State for current active Tab
+  const [currentActiveTab, setCurrentActiveTab] = useState("1");
+  // Toggle active state for Tab
+  const toggle = (tab) => {
+    if (currentActiveTab !== tab) setCurrentActiveTab(tab);
+  };
   return (
     <Box>
-        <Nav tabs>
+      <Nav tabs>
         <NavItem>
-            <NavLink  className={classnames({active:currentActiveTab === '1'})}onClick={() => { toggle('1'); }}>
-            Doctor
-            </NavLink>
+          <NavLink
+            className={classnames({ active: currentActiveTab === "1" })}
+            onClick={() => {
+              toggle("1");
+            }}
+          >
+            Doctor Regular
+          </NavLink>
         </NavItem>
         <NavItem>
-            <NavLink
-            className={classnames({active:currentActiveTab === '2'})} onClick={() => { toggle('2'); }} >
+          <NavLink
+            className={classnames({ active: currentActiveTab === "2" })}
+            onClick={() => {
+              toggle("2");
+            }}
+          >
             Doctor on call
-            </NavLink>
+          </NavLink>
         </NavItem>
-       
-        </Nav>
-        <TabContent activeTab={currentActiveTab}>
-            <TabPane tabId="1">
-                <Row>
-                    <Col sm="12">
-                        <RegFormDR />
-                    </Col>
-                </Row>
-            </TabPane>
-            <TabPane tabId="2">
-                <Row>
-                    <Col sm="12">
-                        <Doc_On_Call />
-                    </Col>
-                </Row>
-            </TabPane>
-        </TabContent>
-     
-           
-        
+      </Nav>
+      <TabContent activeTab={currentActiveTab}>
+        <TabPane tabId="1">
+          <Row>
+            <Col sm="12">
+              <RegFormDR />
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="2">
+          <Row>
+            <Col sm="12">
+              <Doc_On_Call />
+            </Col>
+          </Row>
+        </TabPane>
+      </TabContent>
     </Box>
-    
   );
-} 
+}
 export default FormTabs;
--- src/components/ui/RegForms/RegFormDR.jsx
+++ src/components/ui/RegForms/RegFormDR.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form, FormGroup, Label, Input, Row, Col } from 'reactstrap';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form, FormGroup, Label, Input, Row, Col } from "reactstrap";
 
 function RegFormDR() {
   const handleSubmit = (event) => {
     event.preventDefault();
-  
+
     const {
       doctor_name,
       doctor_id,
@@ -14,12 +14,12 @@ function RegFormDR() {
       salary,
       qualification,
     } = event.target.elements;
-  
+
     // Fetch API call
-    fetch('http://localhost/hMS/PHP/dregister.php', {
-      method: 'POST',
+    fetch("http://localhost/hMS/PHP/dregister.php", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         doctorName: doctor_name.value,
@@ -30,25 +30,25 @@ function RegFormDR() {
         qualification: qualification.value,
       }),
     })
-    .then((response) => response.json())
-    .then((data) => {
-      // Handle the response data
-      if (data.success) {
-        window.alert('Registration successful');
-      } else {
-        if (data.message.includes("Integrity constraint violation")) {
-          window.alert('Register your Doctor ID First!');
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data
+        if (data.success) {
+          window.alert("Registration successful");
         } else {
-          window.alert('Registration failed');
+          if (data.message.includes("Integrity constraint violation")) {
+            window.alert("Register your Doctor ID First!");
+          } else {
+            window.alert("Registration failed");
+          }
         }
-      }
-    })
-    .catch((error) => {
-      // Handle any errors
-      console.error(error);
-      window.alert('An error occurred. Please try again.');
-    });
-  };  
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+        window.alert("An error occurred. Please try again.");
+      });
+  };
 
   return (
     <>
@@ -57,11 +57,12 @@ function RegFormDR() {
           <Col md={4}>
             <FormGroup>
               <Label htmlFor="doctor_name">Name</Label>
-              <Input 
-              id="doctor_name" 
-              name="doctor_name" 
-              placeholder="Full Name" 
-              type="text" />
+              <Input
+                id="doctor_name"
+                name="doctor_name"
+                placeholder="Full Name"
+                type="text"
+              />
             </FormGroup>
           </Col>
           <Col md={3}>
@@ -89,11 +90,7 @@ function RegFormDR() {
           <Col md={3}>
             <FormGroup>
               <Label htmlfor="department_name">Department</Label>
-              <Input 
-              id="department_name" 
-              name="department_name" 
-              type="select"
-              >
+              <Input id="department_name" name="department_name" type="select">
                 <option>Anesthesiology</option>
                 <option>Cardiology</option>
                 <option>Cardiothoracic Surgery</option>
@@ -123,7 +120,11 @@ function RegFormDR() {
           <Col md={5}>
             <FormGroup>
               <Label htmlFor="address">Address</Label>
-              <Input id="address" name="address" placeholder="Enter Complete Address" />
+              <Input
+                id="address"
+                name="address"
+                placeholder="Enter Complete Address"
+              />
             </FormGroup>
           </Col>
           <Col md={4}>
@@ -141,7 +142,7 @@ function RegFormDR() {
             </FormGroup>
           </Col>
         </Row>
-        <Button type="submit">Sign up</Button>
+        <Button type="submit">Save</Button>
       </Form>
     </>
   );
